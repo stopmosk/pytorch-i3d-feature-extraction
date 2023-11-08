3c76c21be169a9df35542278e1481660322c193f
zhangchao <nezhaat78@gmail.com>
2023-11-09 00:12:07 +0800

    update:添加debug信息

--- cal_opt_flow.py
+++ cal_opt_flow.py
@@ -11,6 +11,8 @@ out_root = '/data2/optical_flow'
 
 
 def cal_for_frames(video_path):
+    assert os.path.exists(video_path)
+
     # print(video_path)
     frames = glob.glob(os.path.join(video_path, '*.jpg'))
     frames.sort()
@@ -51,6 +53,7 @@ def save_flow(video_flows, flow_path):
 
 
 def process(video_path, flow_path):
+    print("start processing: ", video_path)
     flow = cal_for_frames(video_path)
     save_flow(flow, flow_path)
 
@@ -63,6 +66,7 @@ def extract_flow(root, out_root):
         dir_list = [x.strip().replace("SPACE", " ") for x in f.readlines() if len(x.strip()) > 0]
 
     pool = multiprocessing.Pool(processes=4)
+    print("len(dir_list): ", len(dir_list))
     for dir_name in dir_list:
         video_path = os.path.join(root, dir_name)
         flow_path = os.path.join(out_root, dir_name)
